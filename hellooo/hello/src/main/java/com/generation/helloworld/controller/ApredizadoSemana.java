1343357d36264609ccfcaf7222e8e4cc4d3736c6
Mariá Dombrate <106116133+dombrate@users.noreply.github.com>
2022-08-05 20:55:10 -0300

    Add files via upload

---
+++ hellooo/hello/src/main/java/com/generation/helloworld/controller/ApredizadoSemana.java
@@ -0,0 +1,28 @@
+package com.generation.helloworld.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Aprendizado") 
+
+public class ApredizadoSemana {
+	@GetMapping
+	public ArrayList <String> Semana() {
+		ArrayList<String> SemanaList = new ArrayList<>();
+		
+		SemanaList.add(" * Lista de Aprendizagem *");
+		SemanaList.add(" - MySQL");
+		SemanaList.add(" - Spring tool");
+		SemanaList.add(" - Responsabilidade pessoal");
+		SemanaList.add(" - Kahoot");
+		SemanaList.add(" - Ter impatia");
+		SemanaList.add(" - Ter atenção");
+		
+		return SemanaList;
+		
+	}
+}
